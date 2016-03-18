b14f2d6654cd374e9f9f52733d8eb304b2435ac7
Marcel Groeneweg <marcel.groeneweg@itvisors.com>
2016-03-18 20:53:58 +0100

    Load hard coded data using function

--- src/DataTables/widget/DataTables.js
+++ src/DataTables/widget/DataTables.js
@@ -60,6 +60,8 @@ define([
         // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
         _handles: null,
         _contextObj: null,
+        _tableNodelist: null,
+        _table: null,
 
         // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
         constructor: function () {
@@ -72,50 +74,17 @@ define([
         // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
         postCreate: function () {
             logger.debug(this.id + ".postCreate");
-            this._updateRendering();
-            this._setupEvents();
             
-            var dataSet = [
-                [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
-                [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
-                [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ],
-                [ "Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060" ],
-                [ "Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700" ],
-                [ "Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000" ],
-                [ "Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500" ],
-                [ "Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900" ],
-                [ "Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500" ],
-                [ "Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600" ],
-                [ "Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560" ],
-                [ "Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ],
-                [ "Charde Marshall", "Regional Director", "San Francisco", "6741", "2008/10/16", "$470,600" ],
-                [ "Haley Kennedy", "Senior Marketing Designer", "London", "3597", "2012/12/18", "$313,500" ],
-                [ "Tatyana Fitzpatrick", "Regional Director", "London", "1965", "2010/03/17", "$385,750" ],
-                [ "Michael Silva", "Marketing Designer", "London", "1581", "2012/11/27", "$198,500" ],
-                [ "Paul Byrd", "Chief Financial Officer (CFO)", "New York", "3059", "2010/06/09", "$725,000" ],
-                [ "Gloria Little", "Systems Administrator", "New York", "1721", "2009/04/10", "$237,500" ],
-                [ "Bradley Greer", "Software Engineer", "London", "2558", "2012/10/13", "$132,000" ],
-                [ "Dai Rios", "Personnel Lead", "Edinburgh", "2290", "2012/09/26", "$217,500" ],
-                [ "Jenette Caldwell", "Development Lead", "New York", "1937", "2011/09/03", "$345,000" ],
-                [ "Yuri Berry", "Chief Marketing Officer (CMO)", "New York", "6154", "2009/06/25", "$675,000" ],
-                [ "Caesar Vance", "Pre-Sales Support", "New York", "8330", "2011/12/12", "$106,450" ],
-                [ "Doris Wilder", "Sales Assistant", "Sidney", "3023", "2010/09/20", "$85,600" ],
-                [ "Angelica Ramos", "Chief Executive Officer (CEO)", "London", "5797", "2009/10/09", "$1,200,000" ],
-                [ "Gavin Joyce", "Developer", "Edinburgh", "8822", "2010/12/22", "$92,575" ],
-                [ "Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "$357,650" ],
-                [ "Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850" ],
-                [ "Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000" ],
-                [ "Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000" ],
-                [ "Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400" ],
-                [ "Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500" ],
-                [ "Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000" ],
-                [ "Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500" ],
-                [ "Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050" ],
-                [ "Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675" ]
-            ];
-
-            $("#example").DataTable({
-                data: dataSet,
+            this._tableNodelist = $("#" + this.domNode.id + " #tableToConvert");
+            
+            this._table = this._tableNodelist.DataTable({
+                serverSide: true,
+                searching: false,
+                ajax: dojoLang.hitch(this, this._getData),
+//                scrollY: 200,
+//                scroller: {
+//                    loadingIndicator: true
+//                },
                 columns: [
                     { title: "Name" },
                     { title: "Position" },
@@ -126,9 +95,10 @@ define([
                 ]
             });
             
-            //$(this.domNode).addClass("display table table-striped table-bordered dataTable");
+            this._updateRendering();
+            this._setupEvents();
         },
-
+            
         // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
         update: function (obj, callback) {
             logger.debug(this.id + ".update");
@@ -159,6 +129,8 @@ define([
         uninitialize: function () {
             logger.debug(this.id + ".uninitialize");
             // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+            this._clearTableData();
+            this._table = null;
         },
 
         // We want to stop events on a mobile device
@@ -202,6 +174,32 @@ define([
 //                }
 //            });
         },
+        
+        // Get data 
+        _getData: function (data, callback, settings) {
+            var dataSet = [
+                [ "Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "page: " + data.start ],
+                [ "Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850" ],
+                [ "Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000" ],
+                [ "Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000" ],
+                [ "Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400" ],
+                [ "Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500" ],
+                [ "Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000" ],
+                [ "Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500" ],
+                [ "Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050" ],
+                [ "Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675" ]
+            ];
+            
+            setTimeout(function () {
+                callback({
+                    draw: data.draw,
+                    data: dataSet,
+                    recordsTotal: 1000,
+                    recordsFiltered: 1000
+                });
+            }, 50);
+            
+        },
 
         // Rerender the interface.
         _updateRendering: function () {
@@ -209,14 +207,22 @@ define([
 //            this.colorSelectNode.disabled = this.readOnly;
 
             if (this._contextObj !== null) {
+                //this._tableNodelist.addClass("display table table-striped table-bordered dataTable");
                 dojoStyle.set(this.domNode, "display", "block");
 
             } else {
                 dojoStyle.set(this.domNode, "display", "none");
+                this._clearTableData();
             }
 
         },
 
+        // Clear table and related objects.
+        _clearTableData: function () {
+            if (this._table) {
+                this._table.clear();
+            }
+        },
 
         // Reset subscriptions.
         _resetSubscriptions: function () {
