2e20ac39d9d09b62af930be8f84bf6ea42d56a7b
Marcel Groeneweg <marcel.groeneweg@itvisors.com>
2017-03-06 18:55:10 +0100

    DataTables OQL views added validations.

--- test/javasource/datatablesutils/DataTablesRequestHandler.java
+++ test/javasource/datatablesutils/DataTablesRequestHandler.java
@@ -3,8 +3,9 @@ package datatablesutils;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.math.NumberUtils;
 import org.json.JSONArray;
@@ -15,10 +16,10 @@ import com.mendix.externalinterface.connector.RequestHandler;
 import com.mendix.logging.ILogNode;
 import com.mendix.m2ee.api.IMxRuntimeRequest;
 import com.mendix.m2ee.api.IMxRuntimeResponse;
+import com.mendix.systemwideinterfaces.connectionbus.data.IDataColumnSchema;
 import com.mendix.systemwideinterfaces.connectionbus.data.IDataRow;
 import com.mendix.systemwideinterfaces.connectionbus.data.IDataTable;
 import com.mendix.systemwideinterfaces.core.IContext;
-import com.mendix.systemwideinterfaces.core.IContext.ExecutionType;
 import com.mendix.systemwideinterfaces.core.ISession;
 
 import datatablesutils.proxies.DataTablesView;
@@ -34,53 +35,128 @@ public class DataTablesRequestHandler extends RequestHandler {
 
 		JSONObject responseJSON = new JSONObject();
 		int responseStatus = IMxRuntimeResponse.OK;
+		JSONArray errorMessageArray = new JSONArray();
+		DataTablesView view = null;
+		Map<String,OqlDataColumn> columnMap = new HashMap<>();
+		ISession session = null;
+		IContext context  = null;
+		IDataTable dataTable = null;
+		boolean isTraceActive = logger.isTraceEnabled();
 		
 		try {
 			// Pull the data from the request.
 			String viewName = getStringParameter(request, "view");
 			String search = getStringParameter(request, "view");
 			String columns = getStringParameter(request, "columns");
-			List<String> columnList = Arrays.asList(columns.split(","));
 			String sort = getStringParameter(request, "sort");
 			int offset = getIntParameter(request, "offset");
 			int limit = getIntParameter(request, "limit");
 			if (limit == 0) {
 				limit = DEFAULT_LIMIT;
 			}
+
+			// Required parameters
+			if (viewName == null || viewName.isEmpty()) {
+				responseStatus = IMxRuntimeResponse.BAD_REQUEST;
+				errorMessageArray.put("The view parameter is required");
+			}
+			if (columns == null || columns.isEmpty()) {
+				responseStatus = IMxRuntimeResponse.BAD_REQUEST;
+				errorMessageArray.put("The columns parameter is required");
+			}
+			if (sort == null || sort.isEmpty()) {
+				responseStatus = IMxRuntimeResponse.BAD_REQUEST;
+				errorMessageArray.put("The sort parameter is required");
+			}
 			
-			ISession session = this.getSessionFromRequest(request);
-			if (session == null) {
-				responseStatus = IMxRuntimeResponse.UNAUTHORIZED;
-			} else {
-				IContext context = session.createContext();
-				List<DataTablesView> viewList = DataTablesView.load(context, "[Name='" + viewName + "']");
-				if (viewList.isEmpty()) {
-					logger.error("View " + viewName + " not found"); 
-					responseStatus = IMxRuntimeResponse.NOT_FOUND;
-				} else {
-					JSONArray resultArray = new JSONArray();
-					responseJSON.put("results", resultArray);
-					DataTablesView view = viewList.get(0);
-					IDataTable dataTable = Core.retrieveOQLDataTable(context, view.getQuery(), limit, offset);
-					int rowIndex = 0;
-					for (IDataRow row : dataTable) {
-						JSONObject rowObject = new JSONObject();
-						resultArray.put(rowObject);
-						rowIndex++;
-						logger.trace("Row " + rowIndex);
-						for (String columnName : columnList) {
-							if (row.hasReadRights(columnName)) {
-								final Object value = row.getValue(context, columnName);
-								logger.trace("  " + columnName + "=" + value);
-								rowObject.put(columnName, value);
-							}
-						}
-					}
-					responseJSON.put("rowCount", dataTable.getRowCount());
+			// Still valid? 
+			if (responseStatus == IMxRuntimeResponse.OK) {
+				
+				//Process requested columns. The names are case sensitive but URL data is not.
+				List<String> columnNameList = Arrays.asList(columns.split(","));
+				for (String columnName : columnNameList) {
+					OqlDataColumn column = new OqlDataColumn();
+					column.setRequestedName(columnName);
+					column.setColumnIndex(-1);
+					columnMap.put(columnName.toLowerCase(), column);
+				}
+				
+				// Get the sessopm
+				session = this.getSessionFromRequest(request);
+				if (session == null) {
+					responseStatus = IMxRuntimeResponse.UNAUTHORIZED;
 				}
 			}
 			
+			// Still valid? Get the view.
+			if (responseStatus == IMxRuntimeResponse.OK) {
+				context = session.createContext();
+				List<DataTablesView> viewList = DataTablesView.load(context, "[Name='" + viewName + "']");
+				if (!viewList.isEmpty()) {
+					view = viewList.get(0);
+				} else {
+					String errorMessage = "View " + viewName + " not found";
+					errorMessageArray.put(errorMessage);
+					logger.error(errorMessage); 
+					responseStatus = IMxRuntimeResponse.NOT_FOUND;
+				}
+			}
+			
+			// Still valid? Get the data and check the requested columns.
+			if (responseStatus == IMxRuntimeResponse.OK) {
+				
+				// Get the data
+				dataTable = Core.retrieveOQLDataTable(context, view.getQuery(), limit, offset);
+				
+				// Find any requested columns and save the data set column index 
+				for (IDataColumnSchema dataColumnSchema : dataTable.getSchema().getColumnSchemas()) {
+					String key = dataColumnSchema.getName().toLowerCase();
+					if (columnMap.containsKey(key)) {
+						OqlDataColumn column = columnMap.get(key);
+						column.setColumnIndex(dataColumnSchema.getIndex());
+					}
+				}
+				
+				// Find any columns that did not get a column index.
+				for (OqlDataColumn column : columnMap.values()) {
+					if (column.getColumnIndex() < 0) {
+						responseStatus = IMxRuntimeResponse.BAD_REQUEST;
+						errorMessageArray.put("Column " + column.getRequestedName() + " does not exist in the data set.");
+					}
+				}
+			}
+				// Still valid? Fill the response.
+			if (responseStatus == IMxRuntimeResponse.OK) {
+				JSONArray resultArray = new JSONArray();
+				responseJSON.put("results", resultArray);
+				int rowIndex = 0;
+				for (IDataRow row : dataTable) {
+					JSONObject rowObject = new JSONObject();
+					resultArray.put(rowObject);
+					rowIndex++;
+					if (isTraceActive) {
+						logger.trace("Row " + rowIndex);
+					}
+					for (OqlDataColumn column : columnMap.values()) {
+						final int columnIndex = column.getColumnIndex();
+						if (columnIndex >= 0) {
+							if (row.hasReadRights(columnIndex)) {
+								final Object value = row.getValue(context, columnIndex);
+								if (isTraceActive) {
+									logger.trace("  " + column.getRequestedName() + "=" + value);
+								}
+								rowObject.put(column.getRequestedName(), value);
+							}
+						}
+					}
+				}
+				responseJSON.put("rowCount", dataTable.getRowCount());
+			}
+			
 			responseJSON.put("status", responseStatus);
+			if (errorMessageArray.length() > 0) {
+				responseJSON.put("errors", errorMessageArray);
+			}
 			response.getWriter().append(responseJSON.toString());
 			
 		} catch (Exception e) {
---
+++ test/javasource/datatablesutils/OqlDataColumn.java
@@ -0,0 +1,22 @@
+package datatablesutils;
+
+public class OqlDataColumn {
+
+	private String requestedName;
+	private int columnIndex;
+	
+	public String getRequestedName() {
+		return requestedName;
+	}
+	public void setRequestedName(String requestedName) {
+		this.requestedName = requestedName;
+	}
+	public int getColumnIndex() {
+		return columnIndex;
+	}
+	public void setColumnIndex(int columnIndex) {
+		this.columnIndex = columnIndex;
+	}
+	
+
+}
